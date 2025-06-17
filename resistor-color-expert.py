4ebcd095debc3610ea0c8371c2308b599d475d93
Konovor <stefan.rothermel@web.de>
2025-06-17 14:02:13 +0200

    Update resistor-color-expert.py
    Messed up code for educational purposes

--- resistor-color-expert.py
+++ resistor-color-expert.py
@@ -1,58 +1,53 @@
-COLOR_TO_VALUE = {
-    'black': 0,
-    'brown': 1,
-    'red': 2,
-    'orange': 3,
-    'yellow': 4,
-    'green': 5,
-    'blue': 6,
-    'violet': 7,
-    'grey': 8,
-    'white': 9,
-}
+def resistor_label(colors):
+    UNIT = 'ohms'
+    codas = []
+    colars = {"black":'0',"brown":'1',"red":'2',"orange":'3',"yellow":'4',"green":'5',"blue":'6',"violet":'7',"grey":'8',"white":'9'}
+    zeros = {"black":0,"brown":1,"red":2,"orange":3,"yellow":4,"green":5,"blue":6,"violet":7,"grey":8,"white":9,}
+    tolers = { "grey": "0.05%","violet":"0.1%","blue":"0.25%","green":" ±0.5%","brown":"1%","red":"2%","gold":"5%", }
+    if len(colors)==1:
+        return "0 ohms"
+    if len(colors)==4:
+        for i in range(0,2):
+            codas.append(colars[colors[i]])
+        plier = zeros[colors[2]]
+        tocod = " ±"+tolers[colors[3]]
+    elif len(colors)==5:
+        for i in range(0,3):
+            codas.append(colars[colors[i]])
+        plier = int(colars[colors[3]])
+        tocod = " ±"+tolers[colors[4]]
 
-COLOR_TO_TOLERANCE = {
-    'grey': '0.05%',
-    'violet': '0.1%',
-    'blue': '0.25%',
-    'green': '0.5%',
-    'brown': '1%',
-    'red': '2%',
-    'gold': '5%',
-    'silver': '10%',
-    }
+    if tocod == " ± ±0.5%":
+        tocod = " ±0.5%"
 
-UNIT = 'ohms'
+    if colors[-1] == "silver":
+        tocod = " ±10%"
 
-def normalize_color_code(colors: list[str]) -> list[str]:
-    if len(colors) == 4:
-        # prepending a zero aligns all further processing of 4 or 5 band resistors
-        colors.insert(0, 'black')
-    return colors
-
-def resistor_label(colors: list[str]) -> str:
-    if len(colors) == 1:
-        return '0 ohms'
-
-    colors = normalize_color_code(colors)
-
-    first_digit = COLOR_TO_VALUE[colors[0]]
-    second_digit = COLOR_TO_VALUE[colors[1]]
-    third_digit = COLOR_TO_VALUE[colors[2]]
-    fourth_digit = COLOR_TO_VALUE[colors[3]]
-    tolerance = COLOR_TO_TOLERANCE[colors[4]]
-
-    resistance_value = (first_digit * 100 + second_digit * 10 + third_digit) * (10 ** fourth_digit)
-
-    # Format resistance without trailing zeros and proper unit
-
-    if resistance_value >= 1_000_000_000:
-        resistance = f"{resistance_value / 1_000_000_000:g} giga"
-    elif resistance_value >= 1_000_000:
-        resistance = f"{resistance_value / 1_000_000:g} mega"
-    elif resistance_value >= 1_000:
-        resistance = f"{resistance_value / 1_000:g} kilo"
+    ccode = "".join(codas)
+    value_ccode = int(ccode)
+    value_ccode = value_ccode*(10 **plier)
+    
+    if value_ccode < 1000:
+        return str(value_ccode)+ " " + UNIT + tocod
     else:
-        resistance = f"{resistance_value:g} "
+        if value_ccode >= 10**9:
+            ohm_val = value_ccode/10**9
+            if ohm_val == int(ohm_val):
+                return f"{int(ohm_val)} gigaohms" + tocod
+            else:
+                return f"{ohm_val} gigaohms" + tocod
+        elif value_ccode >= 10**6:
+            ohm_val = value_ccode/10**6
+            if ohm_val == int(ohm_val):
+                return f"{int(ohm_val)} megaohms" + tocod
+            else:
+                return f"{ohm_val} megaohms" + tocod
+        else:
+            ohm_val = value_ccode/10**3
+            if ohm_val == int(ohm_val):
+                return f"{int(ohm_val)} kiloohms" + tocod
+            else:
+                return f"{ohm_val} kiloohms" + tocod
+    return str(value_ccode)+ " ohms" + tocod
 
-    return f"{resistance}{UNIT} ±{tolerance}"
+    return "0 ohms"
